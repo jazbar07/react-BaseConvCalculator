e1a10e7bbce47e518726e609de1e1303cd273709
Jaz <barnett.jazmine2000@gmail.com>
2024-04-22 20:01:18 -0400

    finished input box use state functionality, able to see inputted values via console

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Nav from './components/Nav';
+import InputBox from './components/InputBox';
+
 function App() {
   return (
     <div className='App'>
       <Nav />
+      <InputBox />
       
     </div>
   )
--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,12 +1,43 @@
+import React, { useState } from 'react';
+import './styles/inputbox.css';
+
 const InputBox = () => {
+    const [input, setInput] = useState ('');
+    const [baseCase, setCase] = useState ('Decimal');
+
+    const handleInput = (e) => {
+        setInput (e.target.value);
+    };
+
+    const handleCase = (e) => {
+        setCase (e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        // keeps page froom doing normal html default response of refreshing when submit
+        e.preventDefault();
+
+        console.log ('Inputed Value', input);
+        console.log ('Base Case:', baseCase);
+    }
+
+
     return (
-        <form className="Input_body">
-            <input type="text" name="input" />
-
-            <input type="submit" value="submit" />
-        </form>
+        <div>
+            <form className="main-input" onSubmit={ handleSubmit }>
+                <input className="input-box" type="text" name="input" 
+                    value={ input } onChange={ handleInput }/>
+                <select className="select-box" name="base cases"
+                    value={ baseCase } onChange={ handleCase } >
+                    <option value="Decimal">Decimal</option>
+                    <option value="Binary">Binary</option>
+                    <option value="Hexadecimal">Hexadecimal</option>
+                    <option value="Octal">Octal</option>
+                </select>
+                <input type="submit" value="submit" />
+            </form>
+        </div>
     );
-
 };
 
 export default InputBox;
